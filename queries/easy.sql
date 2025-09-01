8e22f2e50ffd424b4e706bb49449dddfca890854
angad2212 <angadsuryavanshi22@gmail.com>
2025-09-01 23:38:42 +0530

    easy query 2

--- queries/easy.sql
+++ queries/easy.sql
@@ -2,6 +2,7 @@
 select * from customers where signup_date between '2023-01-15' and '2023-12-31'
 
 -- Get the total number of orders.
+select count(*) as number_of_orders from orders
 
 -- Find the total revenue from the Orders table.
 
